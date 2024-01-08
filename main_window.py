0ff553e503381dad9fa877211116147725d60de5
Mihail Karaev <karaevmihail@gmail.com>
2024-01-08 15:42:31 +0500

    add detection and landmark visualisation

--- main_window.py
+++ main_window.py
@@ -1,8 +1,8 @@
-
-
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QFileDialog
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QFileDialog, QLabel, QHBoxLayout
+from PyQt6.QtGui import QImage, QPixmap
 from processing import *
+import cv2
 
 
 # Подкласс QMainWindow для настройки главного окна приложения
@@ -10,17 +10,59 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.image_processor = ImageProcessor()
         self.setWindowTitle("Оценщик качества")
-        button = QPushButton("Загрузить изображение")
-        button.setCheckable(True)
-        button.clicked.connect(self.load_image)
-        self.setCentralWidget(button)
+        self.load_image_button = QPushButton("Загрузить изображение")
+        self.load_image_button.setCheckable(True)
+        self.load_image_button.clicked.connect(self.load_image)
+        self.setCentralWidget(self.load_image_button)
         self.setFixedSize(QSize(1280, 720))
 
     def load_image(self):
         dialog = QFileDialog(self)
-        filename = dialog.getOpenFileName(self, "Open Image", filter="Images (*.png *.xpm *.jpg)")
-        print(filename)[0]
-        if filename != "":
-            print("Loading image")
+        filename = dialog.getOpenFileName(self, "Open Image", filter="Images (*.png *.xpm *.jpg)")[0]
 
+        if filename != "":
+            self.load_image_button.hide()
+            img = cv2.imread(filename)
+            processing_result = self.image_processor.process(img)
+            boxes, tgt_id, ver_lst, face_qual, bgr_unif = processing_result
+
+            tgt_ver_lst = ver_lst[tgt_id]
+            tgt_box = boxes[tgt_id]
+
+            start_point = (int(tgt_box[0]), int(tgt_box[1]))
+            end_point = (int(tgt_box[2]), int(tgt_box[3]))
+
+            img = cv2.rectangle(img, start_point, end_point, (0, 0, 255), 2)
+
+            for pt in tgt_ver_lst.T:
+                print(pt)
+                img = cv2.circle(img, (int(pt[0]), int(pt[1])), radius=0, color=(255, 0, 0), thickness=3)
+
+            pixmap = QPixmap()
+            height, width, channel = img.shape
+            bytesPerLine = 3 * width
+            q_img = QImage(img.data, width, height, bytesPerLine, QImage.Format.Format_BGR888)
+            pixmap.convertFromImage(q_img)
+
+            # lbl = QLabel(self)
+            # lbl.setPixmap(pixmap)
+            # lbl.move(250,80)
+            # lbl.show()
+            #
+            # hbox = QHBoxLayout(self)
+            # hbox.addWidget(lbl)
+            #
+            # self.setLayout(hbox)
+            # self.show()
+
+            lbl = QLabel(self)
+            lbl.setPixmap(pixmap)
+            hbox = QHBoxLayout(self)
+            hbox.addWidget(lbl)
+            self.setCentralWidget(lbl)
+            self.setLayout(hbox)
+            self.move(300, 200)
+            self.setWindowTitle('image')
+            self.show()
--- processing.py
+++ processing.py
@@ -4,6 +4,7 @@ import sys
 import os
 import numpy as np
 from torch import nn
+from qaa.models import qe_pipeline
 from skimage import transform as trans
 
 sys.path.append("./tddfa_v2/")
@@ -27,7 +28,10 @@ def normalize_face(img, kps):  # performs face alignment
         [62.7299, 92.2041]], dtype=np.float32)
     src[:, 0] += 8.0
 
-    kps = np.array(kps)[:, :2]
+
+    _kps = np.array((np.mean(kps[36:41][:2]), np.mean(kps[42:47][:2])))
+    kps = np.vstack((_kps, kps[33, 48, 54][:2]))
+
     dst = kps.astype(np.float32)
 
     tform = trans.SimilarityTransform()
@@ -66,12 +70,11 @@ def get_bgr_unif(img):
 # Select closest to the center bbox
 def sel_cc_bbox(im_shape, bboxes):
     cy, cx = im_shape[0] / 2, im_shape[1] / 2
-
     clid = 0
-    cldist = 99999
+    cldist = 999999
     for i, bbox in enumerate(bboxes):
-        bcx, bcy = bbox[2] - bbox[0] / 2, bbox[3] - bbox[1] / 2
-        dist = np.sqrt(bcx - cx) + np.sqrt(bcy - cy)
+        bcy, bcx = bbox[2] - bbox[0] / 2, bbox[3] - bbox[1] / 2
+        dist = np.sqrt(np.power((bcx - cx),2) + np.power((bcy - cy),2))
 
         if dist < cldist:
             cldist = dist
@@ -80,7 +83,7 @@ def sel_cc_bbox(im_shape, bboxes):
     return clid
 
 
-class ImageProcessor(nn.module):
+class ImageProcessor(nn.Module):
 
     def __init__(self, config_path='./tddfa_v2/configs/mb1_120x120.yml'):
         super().__init__()
@@ -89,13 +92,15 @@ class ImageProcessor(nn.module):
         self.tddfa = TDDFA_ONNX(**self.cfg)
         self.qaa_model = None
 
-    def process(self, img, use_tracking=False, boxes=None, tgt_box=None, ver_lst=None):
+    def process(self, img, use_tracking=False, boxes=None, tgt_id=None, ver_lst=None):
 
         if use_tracking:
-            tgt_box = self.calc_tracking(img, tgt_box, ver_lst)
+            tgt_box = boxes[tgt_id]
+            tgt_box = self.calc_tracking(img, prev_box=tgt_box, prev_ver_lst=ver_lst)
+            boxes[tgt_id] = tgt_box
         else:
             boxes = self.detect_faces(img)
-            tgt_id = sel_cc_bbox(boxes)
+            tgt_id = sel_cc_bbox(img.shape, boxes)
             tgt_box = boxes[tgt_id]
         ver_lst = self.align_landmarks(img, [tgt_box,])
         face_qual = self.get_face_quality(img, ver_lst)
@@ -110,14 +115,16 @@ class ImageProcessor(nn.module):
         return self.tddfa.recon_vers(param_lst, roi_box_lst, dense_flag=False)
 
     def get_face_quality(self, img, ver_lst):
-        normalized_face = normalize_face(img, ver_lst)
-        quality = self.qaa_model(normalized_face)
-        return quality
+        # normalized_face = normalize_face(img, ver_lst)
+        # quality = self.qaa_model(normalized_face)
+        # return quality
+        return 0
 
     def get_bgr_unif(self, img, ver_lst):
-        doc_format_img = estimate_doc_format(img, ver_lst)
-        bgr_unif_score = get_bgr_unif(doc_format_img)
-        return bgr_unif_score
+        # doc_format_img = estimate_doc_format(img, ver_lst)
+        # bgr_unif_score = get_bgr_unif(doc_format_img)
+        # return bgr_unif_score
+        return 0
 
     def calc_tracking(self, img, prev_box, prev_ver_lst):
         # Use optical flow
